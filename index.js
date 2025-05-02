a3368634c9d787e884a782114cb1f11f49affa0a
vishal123k <vishalk38916@gamil.com>
2025-05-02 20:44:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,116 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const expressError = require("./utils/expressError.js");
+
+const User = require("./models/user.js");
+const listingRouter = require("./routers/listing.js");
+const reviewRouter = require("./routers/review.js");
+const userRouter = require("./routers/user.js");
+
+const Listing = require("./models/listing.js");
+
+const dbUrl = process.env.ATLISTDB || "mongodb://127.0.0.1:27017/wanderlust";
+
+mongoose
+  .connect(dbUrl)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.error("DB connection error:", err);
+  });
+
+// View engine and middleware setup
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
+
+// Session store setup
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
+
+store.on("error", (e) => {
+  console.log("Session store error", e);
+});
+
+const sessionOptions = {
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  },
+};
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+// Passport.js setup
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Global middleware to set locals
+app.use((req, res, next) => {
+  res.locals.sucess = req.flash("sucess");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
+});
+
+// Routes
+app.get("/", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
+app.use("/listings", listingRouter);
+app.use("/listings/:id/review", reviewRouter);
+app.use("/", userRouter);
+
+// 404 handler
+app.all("*", (req, res, next) => {
+  next(new expressError(404, "Page Not Found"));
+});
+
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Something went wrong" } = err;
+  res.status(status).render("error.ejs", { message });
+});
+
+// Start server
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
+
+module.exports = app; // Export the app for testing purposes
+
+
+
